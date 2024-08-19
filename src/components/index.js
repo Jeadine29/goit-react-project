1c9f693bce7887627c1df67edc47d922409741b0
Jeadine29 <143826069+Jeadine29@users.noreply.github.com>
2024-08-20 05:08:59 +0800

    update modal

--- src/components/index.js
+++ src/components/index.js
@@ -4,9 +4,8 @@ export * from './Icon/Icon.jsx';
 export * from './TransactionsChart/TransactionsChart';
 export * from './TransactionsTotal/TransactionsTotal';
 export * from './PieChart/PieChart.jsx';
-export * from './UserSetsModal/UserSetsModal';
 export * from './BgImageWrapper/BgImageWrapper.jsx';
 export * from './RegisterForm/RegisterForm.jsx';
 export * from './Loader/Loader.jsx';
 export * from './Logout/Logout.jsx';
-export * from './ActionButton/ActionButton.jsx'; // Update this if necessary
+export * from './ActionButton/ActionButton.jsx';
