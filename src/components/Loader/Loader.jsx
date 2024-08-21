6ea56cde32485c7a502cc6ee6e7be2bf164889e5
Jeadine29 <143826069+Jeadine29@users.noreply.github.com>
2024-08-21 09:47:53 +0800

    Update Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,14 +1,14 @@
-import { Circles } from 'react-loader-spinner';
+import { Oval } from 'react-loader-spinner';
 import s from './Loader.module.css';
 
 const Loader = ({ className = 'backdrop', width = '80', height = '80' }) => {
   return (
     <div className={s[className]}>
-      <Circles
+      <Oval
         height={height}
         width={width}
         color="#0ef387"
-        ariaLabel="circles-loading"
+        ariaLabel="oval-loading"
         wrapperStyle={{}}
         wrapperClass=""
         className={s.loader}
