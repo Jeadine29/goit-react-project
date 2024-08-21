f7a1cf0879731aa89d5ae0b782c54b9df314fb24
Jeadine29 <143826069+Jeadine29@users.noreply.github.com>
2024-08-21 08:44:07 +0800

    Update Loader

--- src/components/Loader/Loader.jsx
+++ src/components/Loader/Loader.jsx
@@ -1,16 +1,14 @@
-import { Watch } from 'react-loader-spinner';
+import { Circles } from 'react-loader-spinner';
 import s from './Loader.module.css';
 
 const Loader = ({ className = 'backdrop', width = '80', height = '80' }) => {
   return (
     <div className={s[className]}>
-      <Watch
-        visible={true}
+      <Circles
         height={height}
         width={width}
-        radius="48"
         color="#0ef387"
-        ariaLabel="watch-loading"
+        ariaLabel="circles-loading"
         wrapperStyle={{}}
         wrapperClass=""
         className={s.loader}
